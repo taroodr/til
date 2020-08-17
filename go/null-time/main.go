4696d2bb2596606a8ce41209ae26c49132f70c19
taroodr <n.kotaro.0101@gmail.com>
2020-08-17 10:02:08 +0900

    unmarshal時の動作を素振り

--- go/null-time/main.go
+++ go/null-time/main.go
@@ -10,11 +10,11 @@ func main() {
 	nullTime := null.Time{}
 	fmt.Println(nullTime) // {0001-01-01 00:00:00 +0000 UTC false}
 	fmt.Println(nullTime.MarshalText()) // [110 117 108 108] <nil>
-	byte, err := nullTime.MarshalText()
+	buffer, err := nullTime.MarshalText()
 	if err != nil {
 		fmt.Println("err", err)
 	}
-	s := string(byte)
+	s := string(buffer)
 	fmt.Printf("%v type:%T\n", s, s) // null type:string,
 
 
@@ -24,10 +24,25 @@ func main() {
 	}
 	fmt.Println(time) // {2020-08-14 14:09:59.624455 +0900 JST m=+0.000755212 true}
 	fmt.Println(time.MarshalText()) // [50 48 50 48 45 48 56 45 49 52 84 49 52 58 48 57 58 53 57 46 54 50 52 52 53 53 43 48 57 58 48 48] <nil>
-	byte2, err := time.MarshalText()
+	buffer2, err := time.MarshalText()
 	if err != nil {
 		fmt.Println("err", err)
 	}
-	s2 := string(byte2)
+	s2 := string(buffer2)
 	fmt.Printf("%v type:%T\n", s2, s2) // 2020-08-14T14:09:59.624455+09:00 type:string
+
+
+	nullableTime := null.Time{}
+	err = nullableTime.UnmarshalText([]byte("2020-08-14T14:09:59.624455+09:00"))
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	fmt.Println(nullableTime) // {2020-08-14 14:09:59.624455 +0900 JST true}
+
+	nullableTime2 := null.Time{}
+	err = nullableTime2.UnmarshalText([]byte("null"))
+	if err != nil {
+		fmt.Println("err", err) // err parsing time "null" as "2006-01-02T15:04:05Z07:00": cannot parse "null" as "2006"
+	}
+	fmt.Println(nullableTime2) // {0001-01-01 00:00:00 +0000 UTC false} エラーになるのでゼロ値のまま
 }
